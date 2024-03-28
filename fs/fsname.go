e6f98c1de6309f498662421879145811d044ae2a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-28 09:21:25 -0700

    refactored a bit

--- fs/fsname.go
+++ fs/fsname.go
@@ -10,25 +10,27 @@ const  defaultWebPageExtension string = ".html"
 func fsName(name string, defaultStem string, defaultExtension string) string {
 	name = path.Canonical("/" + name)
 
-	if "" == defaultStem {
-		defaultStem = defaultWebPageStem
-	}
 	if "" == defaultExtension {
 		defaultExtension = defaultWebPageExtension
 	}
-
-	var defaultFileName string = defaultStem + defaultExtension
 
 	{
 		var last int = len(name) - 1
 
 		if '/' == name[last] {
+
+			if "" == defaultStem {
+				defaultStem = defaultWebPageStem
+			}
+
+			var defaultFileName string = defaultStem + defaultExtension
+
 			name  = path.Join(name, defaultFileName)
 		}
+	}
 
-		if '/' == name[0] {
-			name = name[1:]
-		}
+	if '/' == name[0] {
+		name = name[1:]
 	}
 
 	{
